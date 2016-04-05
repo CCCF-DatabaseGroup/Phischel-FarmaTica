91461c90531ab87740c6a5074abaaccfbe873ec6
thechancher <thechancher@gmail.com>
2016-04-04 22:05:52 -0600

    solo prueba

--- Phisel-Farmatica/Phisel-Farmatica/Controllers/HomeController.cs
+++ Phisel-Farmatica/Phisel-Farmatica/Controllers/HomeController.cs
@@ -58,6 +58,7 @@ namespace Phisel_Farmatica.Controllers
                 }
                 else
                 {
+                    /// new comm
                     return View("~/Views/Home/SuperAdministratorHome.cshtml");
                 }
                 
